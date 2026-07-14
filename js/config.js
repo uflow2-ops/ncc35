b52e19675b6bafc78dd09f99f3f9353e7de55d7a
Choco <uflow2@gmail.com>
2026-07-14 11:47:29 +0900

    123

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 ﻿﻿const CLASS_CONFIG = {
     neisApiKey: '선생님의_나이스_API_키',
-    dahandinApiKey: '8f0823e1243604bfc6a861a04217880d97aa13ddaea95bdd',
+    dahandinApiKey: '94d51472cbeb889a8d4e3888fbb735bcf062d0ceccc1af2c',
     weatherKey: '선생님의_기상청_API_키',
     
     school: {
